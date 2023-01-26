3c43eecf90b04404cbc1823c912c84382dc26748
Thib <clement.thibaut.guillaume@gmail.com>
2023-01-26 14:22:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exoIonic_profil',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
